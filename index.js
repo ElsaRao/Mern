dce46e3abcf64cefbf91a4a89d2cca7889b18215
ElsaRao <elsazehra2002@gmail.com>
2022-04-18 12:30:19 +0500

    added index.js

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ let userAge = 35;
 let currentYear = 2022;
 let birthYear = currentYear-userAge;
 console.log(birthYear)
+
+for(let i = 1; i <= 5; i++){
+    setTimeout(() => {
+        console.log(i)
+    }, i*1000)
+}
